adef81763e01bea00d9ed249ecd31e0116880ed4
Hans-Jörg Bibiko <hans_joerg_bibiko@eva.mpg.de>
2022-07-06 13:37:17 +0200

    updated pytests

--- vanuatuvoices/tests/test_functional.py
+++ vanuatuvoices/tests/test_functional.py
@@ -1,4 +1,27 @@
-def test_home(app):
-    app.get_html('/', status=200)
-    app.get_html('/credits', status=200)
+import pytest
+
+
+@pytest.mark.parametrize(
+    "method,path",
+    [
+        ('get_html', '/'),
+        ('get_html', '/credits'),
+        ('get_html', '/sources'),
+        ('get_html', '/sources/Shimelman2019.snippet.html'),
+        ('get_html', '/languages'),
+        ('get_html', '/languages/Atchinorap'),
+        ('get_html', '/languages/Atchinorap#ipa'),
+        ('get_html', '/languages/Atchinorap?__locale__=eo'),
+        ('get_dt', '/languages?iSortingCols=1&iSortCol_0=1'),
+        ('get_html', '/parameters'),
+        ('get_html', '/parameters/18_i'),
+        ('get_html', '/values'),
+        ('get_html', '/contributors'),
+        ('get_html', '/contributions'),
+        ('get_html', '/valuesets'),
+        ('get_html', '/values/Tiraxorap-1_one-1'),
+        ('get_dt', '/parameters?sSearch_2=right'),
+    ])
+def test_pages(app, method, path):
+    getattr(app, method)(path)
 
